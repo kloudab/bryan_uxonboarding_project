bfc83f6d8aec45166ad5bbefa95cc529cd084819
Bryan Klouda <bryan.klouda@banno.com>
2015-01-09 12:58:46 -0600

    Adds buttons to minimize either section of contact info.

---
+++ script.js
@@ -0,0 +1,15 @@
+angular.module("UserProfileApp", [])
+.controller('UserProfileController', function($scope) {
+    $scope.profileVisibility = {
+        showName: true,
+        showContact: true
+    };
+    
+    $scope.toggleContactInfo = function() {
+        $scope.profileVisibility.showContact = ( $scope.profileVisibility.showContact === false ? true : false);
+    };
+    
+    $scope.toggleName = function() {
+        $scope.profileVisibility.showName = ( $scope.profileVisibility.showName === false ? true : false);
+    };
+});
