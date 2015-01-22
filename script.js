1576b84c98a12141dec7446e4e76657029ac35b2
Bryan Klouda <bryan.klouda@banno.com>
2015-01-22 15:58:28 -0600

    Moves userProfileView directive to bottom of file. Creates angular services for following user actions: create, get (specific user by id), get all users, update user, delete user and creates factories specific to each user action.

--- script.js
+++ script.js
@@ -30,35 +30,69 @@ UserProfileApp.controller('UserProfileController', function($scope, $resource) {
   };
 });
 
-UserProfileApp.directive('userProfileView', function() {
-  return {
-    restrict: 'A',
-    templateUrl: './assets/templates/userProfileView.html'
-  };
-});
-
-UserProfileApp.factory('UsersFactory', function($resource) {
+UserProfileApp.factory('getUsersFactory', function($resource) {
   return $resource(
-    'localhost:24149/users',
+    'http://localhost:24149/users',
     null,
     {get: {method: 'GET', isArray: true}}
   );
 });
 
-UserProfileApp.factory('UsersService', function(UsersFactory) {
+UserProfileApp.factory('addUserFactory', function($resource) {
+  return $resource(
+    'http://localhost:24149/users',
+    null,
+    {save: {method: 'POST'}}
+  );
+});
+
+UserProfileApp.factory('updateUserFactory', function($resource) {
+  return $resource(
+    'http://localhost:24149/users/:id',
+    null,
+    {update: {method: 'PUT'}}
+  );
+});
+
+UserProfileApp.factory('deleteUserFactory', function($resource) {
+  return $resource(
+    'http://localhost:24149/users/:id',
+    null,
+    {delete: {method: 'DELETE'}}
+  );
+});
+
+UserProfileApp.factory('UsersService', function(getUsersFactory,addUserFactory,
+                                              updateUserFactory, deleteUserFactory) {
   function getUsers() {
-    return UsersFactory.get().$promise;
+    return getUsersFactory.get().$promise;
   }
+  
+  function addUser(user) {
+    return addUserFactory.save(user).$promise;
+  }
+  
+  function updateUser(user) {
+    return updateUserFactory.update(user).$promise;
+  }
+  
+  function deleteUser(user) {
+    return deleteUserFactory.delete(user).$promise;
+  }
+  
   return {
-    get: getUsers
+    get: getUsers,
+    post: addUser,
+    put: updateUser,
+    delete: deleteUser
   };
 });
 
 UserProfileApp.controller('usersController', function($scope, UsersService) {
   UsersService.get().then(function(usersList) {
     $scope.users = usersList;
-  }, function(errorMessage) {
-    console.log(errorMessage);
+  }, function(error) {
+    console.log(error);
   });
 });
   
@@ -73,5 +107,12 @@ UserProfileApp.directive('usersListingView', function() {
   return {
     restrict: 'E',
     templateUrl: './assets/templates/usersListingView.html'
+  };
+});
+
+  UserProfileApp.directive('userProfileView', function() {
+  return {
+    restrict: 'A',
+    templateUrl: './assets/templates/userProfileView.html'
   };
 });
