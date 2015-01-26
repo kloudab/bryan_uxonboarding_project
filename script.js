b7d6572222f3cf02136d82130d0acc28663a8146
Bryan Klouda <bryan.klouda@banno.com>
2015-01-26 11:14:07 -0600

    Renames UserProfileController to UserProfileViewController and moves the controller to the controllers directory in assets/scripts

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
 var UserProfileApp = angular.module('UserProfileApp', ['ui.router', 'ngResource']);
 
-UserProfileApp.controller('UserProfileController', function($scope) {
-  $scope.profileVisibility = {
-    showName: true,
-    showContact: true
-  };
-
-  $scope.toggleContactInfo = function() {
-    $scope.profileVisibility.showContact = ($scope.profileVisibility.showContact === false ? true : false);
-  };
-
-  $scope.toggleName = function() {
-    $scope.profileVisibility.showName = ($scope.profileVisibility.showName === false ? true : false);
-  };
-});
-
 UserProfileApp.factory('getUsersFactory', function($resource) {
   return $resource(
     'http://localhost:24149/users',
