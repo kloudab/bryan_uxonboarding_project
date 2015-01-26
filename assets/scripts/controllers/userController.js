994cebccc72be6e91bfb8ca8f5ec5a127bb52798
Bryan Klouda <bryan.klouda@banno.com>
2015-01-26 16:22:02 -0600

    Adds userController.js

---
+++ assets/scripts/controllers/userController.js
@@ -0,0 +1,36 @@
+UserProfileApp.controller('userController', function($scope, UsersService) {
+
+	var getUser = function(id) { 
+		UsersService.getUser(id).then(function(user) {
+			$scope.user = user;
+		}, function(error) {
+			console.log(error);
+		});
+	};
+
+	var updateUser = function(id) {
+		UsersService.updateUser(id).then(function() {
+			console.log("User Successfully Updated!");
+		}, function(error) {
+			console.log(error);
+		});
+	};
+
+	var removeUser = function(id) {
+		UsersService.removeUser(id).then(function() {
+      console.log("User Successfully Removed!");
+		}, function(error) {
+			console.log(error);
+		});
+	};
+
+	var createUser = function(user) {
+		UsersService.createUser(user).then(function(user) {
+			console.log(user)
+      console.log("Successfully Created!");
+		}, function(error) {
+			console.log(error);
+		});
+	};
+
+});
