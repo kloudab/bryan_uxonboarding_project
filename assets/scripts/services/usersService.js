ac61c6085d0075960e28daec261ad78b7510c371
Bryan Klouda <bryan.klouda@banno.com>
2015-01-26 12:46:50 -0600

    Moves UsersService to services directory from script.js

---
+++ assets/scripts/services/usersService.js
@@ -0,0 +1,25 @@
+UserProfileApp.factory('UsersService', function(getUsersFactory,addUserFactory,
+                                              updateUserFactory, removeUserFactory) {
+  function getUsers() {
+    return getUsersFactory.get().$promise;
+  }
+
+  function addUser(user) {
+    return addUserFactory.save(user).$promise;
+  }
+
+  function updateUser(user) {
+    return updateUserFactory.update(user).$promise;
+  }
+
+  function removeUser(user) {
+    return removeUserFactory.remove(user).$promise;
+  }
+
+  return {
+    get: getUsers,
+    post: addUser,
+    put: updateUser,
+    remove: removeUser
+  };
+});
--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-UserProfileApp.factory('UsersService', function(getUsersFactory,addUserFactory,
-                                              updateUserFactory, removeUserFactory) {
-  function getUsers() {
-    return getUsersFactory.get().$promise;
-  }
-
-  function addUser(user) {
-    return addUserFactory.save(user).$promise;
-  }
-
-  function updateUser(user) {
-    return updateUserFactory.update(user).$promise;
-  }
-
-  function removeUser(user) {
-    return removeUserFactory.remove(user).$promise;
-  }
-
-  return {
-    get: getUsers,
-    post: addUser,
-    put: updateUser,
-    remove: removeUser
-  };
-});
-
 UserProfileApp.controller('usersController', function($scope, UsersService) {
   UsersService.get().then(function(usersList) {
     $scope.users = usersList;
