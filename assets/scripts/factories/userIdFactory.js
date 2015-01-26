20ad855eae9e5ca0433b243dac8bff77cfc8b783
Bryan Klouda <bryan.klouda@banno.com>
2015-01-26 13:34:00 -0600

    Fixes syntax error in both factories. Was a missing comma

--- assets/scripts/factories/userIdFactory.js
+++ assets/scripts/factories/userIdFactory.js
@@ -2,7 +2,7 @@ UserProfileApp.factory('userIdFactory', function($resource) {
   return $resource(
     'http://localhost:24149/users/:id',
     null,
-    {update: {method: 'PUT'}
-    remove: {method: 'DELETE'}}
+    {update: {method: 'PUT'},
+     remove: {method: 'DELETE'}}
   );
 });
