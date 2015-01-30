27d794c35192d03e4020010399f03416391a6554
Bryan Klouda <bryan.klouda@banno.com>
2015-01-30 14:55:20 -0600

    Adds placeholder object for use in populating placeholders for userCreateProfileForm. Uses clearForm to clear form when button is clicked and when user successfully creates a profile

--- assets/scripts/controllers/userController.js
+++ assets/scripts/controllers/userController.js
@@ -1,5 +1,12 @@
 UserProfileApp.controller('userController', function($scope, usersService) {
 
+	$scope.placeholder = {
+		firstName : "Your first name here",
+		lastName : "Your last name here",
+		phone : "Your phone number here",
+		email : "Your email address here"
+	};
+
 	$scope.getUser = function(id) { 
 		usersService.getUser(id).then(function(user) {
 			$scope.user = user;
@@ -27,9 +34,15 @@ UserProfileApp.controller('userController', function($scope, usersService) {
 	$scope.createUser = function(user) {
 		usersService.createUser(user).then(function(user) {
      		console.log("Successfully Created!");
+     		alert("User Successfully Created!")
+     		$scope.clearForm();
 		}, function(error) {
 			console.log(error);
 		});
 	};
 
+	$scope.clearForm = function() {
+		$scope.createProfileForm.$setPristine();
+		$scope.user = {};
+	}
 });
