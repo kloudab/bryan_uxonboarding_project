2564305a791d152ab0eb97ff0450358930d2f6dc
Bryan Klouda <bryan.klouda@banno.com>
2015-02-20 10:36:58 -0600

    Starts debugging passing id param to edit view

--- assets/scripts/routes/routes.js
+++ assets/scripts/routes/routes.js
@@ -1,33 +1,33 @@
 UserProfileApp.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
-    .state('home', {
-      url: '/',
-    })
-.state('profile_view', {
-      url: '/profile_view/:id',
-      templateUrl: './assets/templates/userProfileView.html',
-      controller: function($scope, $stateParams) {
-        $scope.id = $stateParams.id;
-      },
-      resolve:{
-        id: ['$stateParams', function($stateParams){
-          return $stateParams.id;
-        }]
-      }
-    })
-    .state('profile_view.edit', {
-      url: '^/edit/:id',
-      templateUrl: './assets/templates/userEditingProfileView.html',
-      controller: function($scope, $stateParams, id) {
-        $scope.id = id;
-      }
-    })
-    .state('search_users', {
-      url: '/search_users',
-      templateUrl: './assets/templates/userSearchView.html',
-    })
-    .state('create_user', {
-      url: '/create_user',
-      templateUrl: './assets/templates/userCreateProfileView.html',
-    });
+  .state('home', {
+    url: '/',
+  })
+  .state('profile', {
+    url: '/profile/:id',
+    templateUrl: './assets/templates/userProfileView.html',
+    controller: function($scope, $stateParams) {
+      $scope.id = $stateParams.id;
+    },
+    resolve: {id: ['$stateParams', function($stateParams){
+        return $stateParams.id
+      }]
+    }
+  })
+  .state('profile.edit', {
+    url: '/edit/',
+    templateUrl: './assets/templates/userEditingProfileView.html',
+    controller: function($scope, $stateParams, id) {
+      $scope.id = id;
+      // console.log($stateParams.id);
+    }
+  })
+  .state('search_users', {
+    url: '/search_users',
+    templateUrl: './assets/templates/userSearchView.html',
+  })
+  .state('create_user', {
+    url: '/create_user',
+    templateUrl: './assets/templates/userCreateProfileView.html',
+  });
 });
