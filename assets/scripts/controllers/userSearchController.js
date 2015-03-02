c0854c12d04515dd4abf64b6da8a0f59b8e31490
Bryan Klouda <bryan.klouda@banno.com>
2015-03-02 07:49:39 -0600

    removes commented out code and a property edition in changeBackground

--- assets/scripts/controllers/userSearchController.js
+++ assets/scripts/controllers/userSearchController.js
@@ -12,11 +12,7 @@ UserProfileApp.controller('userSearchController', function($scope, usersService)
                        "./assets/gifs/waves.gif"];
     var randomIndex = Math.floor((Math.random()*backgrounds.length));
     var randomImage = backgrounds[randomIndex];
-    var url = "url('" + randomImage + "')"
-    var property = url + " 1000%";
-    document.body.style.background = property
-    // var element = document.getElementById('main')
-    // element.style.backgroundImage = url;
-    // element.style.backgroundSize = '10%';
+    var background = "url('" + randomImage + "')"
+    document.body.style.background = background
   }
 });
